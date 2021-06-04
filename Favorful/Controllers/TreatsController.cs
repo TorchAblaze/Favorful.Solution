9458b312d195f14f42af77a9f9173ff56d3ed5fc
TorchAblaze <tiffanygreathead@gmail.com>
2021-06-04 16:35:34 -0700

    Add DeleteFlavor functionality in TreatsController

--- Favorful/Controllers/TreatsController.cs
+++ Favorful/Controllers/TreatsController.cs
@@ -111,5 +111,14 @@ namespace Favorful.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }

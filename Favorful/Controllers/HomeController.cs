c493c39d096469f2f06f2b2a00eed0fda98e8d6c
TorchAblaze <tiffanygreathead@gmail.com>
2021-06-04 11:17:11 -0700

    Add Home Index view and controller with styling

--- Favorful/Controllers/HomeController.cs
+++ Favorful/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Favorful.Models;
 
 namespace Favorful.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FavorfulContext _db;
+
+    public HomeController(FavorfulContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Treat> treatList = _db.Treats.ToList();
+      List<Flavor> flavorList = _db.Flavors.ToList();
+      model.Add("Treats", treatList);
+      model.Add("Flavors", flavorList);
+      return View(model);
     }
   }
 }
